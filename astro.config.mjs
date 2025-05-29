5bb16b3883b77ccb7ba32d552ec98439a1c2658d
Bargav Kondapu <bhar1red@gmail.com>
2025-05-29 11:22:28 -0400

    Update astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,33 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://bkdevlabs.com',
   integrations: [
     mdx(),
     sitemap(),
-    tailwind({
-      applyBaseStyles: false,
-    })
+    tailwind()
   ],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
       wrap: true
+    }
+  },
+  build: {
+    format: 'directory',
+    assets: 'assets' // Changed from default '_astro' to 'assets'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/js/[name].[hash].js',
+          chunkFileNames: 'assets/js/[name].[hash].js',
+          assetFileNames: 'assets/[ext]/[name].[hash][extname]'
+        }
+      }
     }
   }
 });
